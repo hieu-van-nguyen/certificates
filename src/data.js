61f243ff2aaa5c8d50a579540e5a6d9504b92260
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-04-05 00:15:40 -0500

    add new pages

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const menu = [
+  {
+    id: 1,
+    title: "Introduction-to-Front-End-Development",
+    category: "FrontEnd",
+    img: "/images/item-1.jpeg",
+    desc: `The core and underlying technologies that power the internet `
+  },
+  {
+    id: 2,
+    title: "Programming-with-JavaScript",
+    category: "FrontEnd",
+    img: "/images/item-2.jpeg",
+    desc: `Foundational programming skills with JavaScript `
+  },
+  {
+    id: 3,
+    title: "Introduction-to-Back-End-Development",
+    category: "BackEnd",
+    img: "/images/item-3.jpeg",
+    desc: `The core and underlying technologies that power the internet.`
+  },
+  {
+    id: 4,
+    title: "Programming-in-Python",
+    category: "BackEnd",
+    img: "/images/item-4.jpeg",
+    desc: `Foundational programming skills with Python `
+  }
+];
+export default menu;
