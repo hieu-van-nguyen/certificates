f01a9a84fed5a62fedf2e715655a9ca866d0ada3
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-05-21 22:28:07 -0500

    update certificates

--- src/data.js
+++ src/data.js
@@ -3,28 +3,28 @@ const menu = [
     id: 1,
     title: "Introduction-to-Front-End-Development",
     category: "FrontEnd",
-    img: "/images/item-1.jpeg",
+    img: "/images/item-1.png",
     desc: `The core and underlying technologies that power the internet `
   },
   {
     id: 2,
     title: "Programming-with-JavaScript",
     category: "FrontEnd",
-    img: "/images/item-2.jpeg",
+    img: "/images/item-2.png",
     desc: `Foundational programming skills with JavaScript `
   },
   {
     id: 3,
     title: "Introduction-to-Back-End-Development",
     category: "BackEnd",
-    img: "/images/item-3.jpeg",
+    img: "/images/item-3.png",
     desc: `The core and underlying technologies that power the internet.`
   },
   {
     id: 4,
     title: "Programming-in-Python",
     category: "BackEnd",
-    img: "/images/item-4.jpeg",
+    img: "/images/item-4.png",
     desc: `Foundational programming skills with Python `
   }
 ];
