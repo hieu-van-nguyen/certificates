1a7a25650cea10293cbded43bd21898a80f86f89
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-05-22 17:03:19 -0500

    update pdf resources

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,29 @@
 import React from "react";
+import IntroductionToFrontEndDevelopment from "./pdf/Introduction-to-Front-End-Development.pdf";
+import ProgrammingWithJavaScript from "./pdf/Programming-with-JavaScript.pdf";
+import IntroductionToBackEndDevelopment from "./pdf/Introduction-to-Back-End-Development.pdf";
+import ProgrammingInPython from "./pdf/Programming-in-Python.pdf";
 
 const Menu = ({ items }) => {
+  const getCertificate = (certificate) => {
+    switch (certificate) {
+      case "Introduction-to-Front-End-Development.pdf":
+        return IntroductionToFrontEndDevelopment;
+      case "Programming-with-JavaScript.pdf":
+        return ProgrammingWithJavaScript;
+      case "Introduction-to-Back-End-Development.pdf":
+        return IntroductionToBackEndDevelopment;
+      case "Programming-in-Python.pdf":
+        return ProgrammingInPython;
+      default:
+        return IntroductionToFrontEndDevelopment;
+    }
+  };
+
   return (
     <div className="section-center">
       {items.map((menuItem) => {
-        const { id, title, img, desc } = menuItem;
+        const { id, title, img, desc, certificate } = menuItem;
         return (
           <article key={id} className="menu-item">
             <img
@@ -17,6 +36,14 @@ const Menu = ({ items }) => {
                 <h4>{title}</h4>
               </header>
               <p className="item-text">{desc}</p>
+              <a
+                onClick={() => {
+                  const certificatePdf = getCertificate(certificate);
+                  window.open(certificatePdf);
+                }}
+              >
+                View Certificate
+              </a>
             </div>
           </article>
         );
--- src/data.js
+++ src/data.js
@@ -4,28 +4,32 @@ const menu = [
     title: "Introduction-to-Front-End-Development",
     category: "FrontEnd",
     img: "/images/item-1.png",
-    desc: `The core and underlying technologies that power the internet `
+    desc: `The core and underlying technologies that power the internet `,
+    certificate: "Introduction-to-Front-End-Development.pdf"
   },
   {
     id: 2,
     title: "Programming-with-JavaScript",
     category: "FrontEnd",
     img: "/images/item-2.png",
-    desc: `Foundational programming skills with JavaScript `
+    desc: `Foundational programming skills with JavaScript `,
+    certificate: "Programming-with-JavaScript.pdf"
   },
   {
     id: 3,
     title: "Introduction-to-Back-End-Development",
     category: "BackEnd",
     img: "/images/item-3.png",
-    desc: `The core and underlying technologies that power the internet.`
+    desc: `The core and underlying technologies that power the internet.`,
+    certificate: "Introduction-to-Back-End-Development.pdf"
   },
   {
     id: 4,
     title: "Programming-in-Python",
     category: "BackEnd",
     img: "/images/item-4.png",
-    desc: `Foundational programming skills with Python `
+    desc: `Foundational programming skills with Python `,
+    certificate: "Programming-in-Python.pdf"
   }
 ];
 export default menu;
